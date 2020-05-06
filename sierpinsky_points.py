3868370b8c4dfb7c26b4da95ade4c2663ec1827e
jydarack <jydarack@gmail.com>
2020-05-06 23:06:52 +0900

    It works

--- sierpinsky_points.py
+++ sierpinsky_points.py
@@ -26,8 +26,8 @@ class Triangle:
         k.plot(x, y, ".", linestyle="None")
 
     def display_fill(self, ax=None):
-        t = plt.Polygon(self.get_vertex())
-        # clr = "black" if self.id else "red"
+        clr = "blue" if self.id == 0 else "yellow"
+        t = plt.Polygon(self.get_vertex(), color=clr, alpha=0.2)
         if ax is None:
             plt.gca().add_patch(t)
         else:
@@ -35,31 +35,29 @@ class Triangle:
 
     def get_sub_triangles(self):
         mp1, mp2, mp3 = self.get_midpoints()
-        t1 = Triangle([self.vertex1, mp1, mp3])
-        t2 = Triangle([self.vertex2, mp1, mp2])
-        t3 = Triangle([self.vertex3, mp2, mp3])
+        t1 = Triangle([self.vertex1, mp1, mp3], id=self.id + 1)
+        t2 = Triangle([self.vertex2, mp1, mp2], id=self.id + 1)
+        t3 = Triangle([self.vertex3, mp2, mp3], id=self.id + 1)
         return t1, t2, t3
 
 
-def sierpinsky(triangle: Triangle = None, depth: int = 5):
+def sierpinsky(ax, triangle: Triangle = None, depth: int = 5):
     if depth < 1:
         return [triangle]
     st = triangle.get_sub_triangles()
     out = [triangle]
     for t in st:
-        out += sierpinsky(t, depth - 1)
+        out += sierpinsky(ax=ax, triangle=t, depth=depth - 1)
+        t.display_fill(ax=ax)
     return out
 
 
 if __name__ == "__main__":
-    t = Triangle([(0, 0), (1, 0), (0.5, 0.75)])
+    t = Triangle([(0, 0), (1, 0), (0.5, 1)], id=5)
 
     fig, ax = plt.subplots()
     # t.display(ax=ax)
-    k = sierpinsky(t, depth=5)
+    k = sierpinsky(ax=ax, triangle=t, depth=7)
     for st in k:
-        # st.display_fill(ax=ax)
-        st.display(ax=ax)
+        st.display_fill(ax=ax)
     plt.show()
-    # print([x.get_vertex() for x in k])
-    # print(k)
